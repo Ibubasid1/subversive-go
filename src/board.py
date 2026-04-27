205ed0157819615af99189a94e848edfe60e6ec6
Ibubasid1 <siddiqui.muhibullah@gmail.com>
2026-04-27 07:45:32 +0500

    New branch

--- src/board.py
+++ src/board.py
@@ -10,7 +10,7 @@ class Board:
     def move(move):
         if(move == 2 && last_move == 2) #end
         else
-            
+
 
     #if even number of moves, then black turn
     #if odd number of moves, then white turn
@@ -18,3 +18,5 @@ class Board:
     #let's call move 1 place and move 2 pass. If there are two consecutive move 2s, the game ends. 
     #however, the ai will likely not willingly pass. The random move will definitely not since that won't even be an option.
     #so, it's more likely that the game will end by the move limit we set, which will be 400
+
+    #have created a new branch for this code, which will specifically be restricted to this file
