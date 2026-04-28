0463ad65b25bbe591c209c527323d68ee8be16df
Ibubasid1 <siddiqui.muhibullah@gmail.com>
2026-04-29 01:44:53 +0500

    Added traversal function

--- src/board.py
+++ src/board.py
@@ -5,20 +5,21 @@ class Board:
     def __init__(self):
         self.board = [[0]*Board.SIZE for _ in range(Board.SIZE)]
         self.moves = 0
-        self.last_move = False
+        self.last_was_pass = False
         self.current_player = 1
+        self.game_over = False
 
     
     def skip(self):
-        if(self.last_move): #checks if opponent has already passed
-            pass #if opponent has, then the game should end
+        if self.last_was_pass: #checks if opponent has already passed
+            self.game_over = True #if opponent has, then the game should end
         else: #if the opponent hasn't, then the game continues 
             self.moves += 1
-            self.last_move = True
+            self.last_was_pass = True
 
     def place(self, row, col): #uses a simply row and column to make the move
         #make a check to see whether the move is available
-        if(self.board[row][col] == 1):
+        if self.board[row][col] == 1 or self.board[row][col] == 2:
             print("Invalid move")
         else:
             self.board[row][col] = self.current_player
@@ -33,20 +34,40 @@ class Board:
             rows.append("|".join(str(cell) for cell in row))
         return "\n".join(rows)
 
-    # def _traversal(self, row, col):
-    #     run = True
-    #     while run:
-    #         if self.board[row + 1][col] == 
+    def _traversal_helper(self, row, col, color, visited) -> set:
+        visited.add((row, col))
+        if (row + 1) < self.SIZE and self.board[row + 1][col] == color and (row + 1, col) not in visited:
+            self._traversal_helper(row + 1, col, color, visited)
+        if (row - 1) >= 0 and self.board[row - 1][col] == color and (row - 1, col) not in visited:
+            self._traversal_helper(row - 1, col, color, visited)
+        if (col + 1) < self.SIZE and self.board[row][col + 1] == color and (row, col + 1) not in visited:
+            self._traversal_helper(row, col + 1, color, visited)
+        if (col - 1) >= 0 and self.board[row][col - 1] == color and (row, col - 1) not in visited:
+            self._traversal_helper(row, col - 1, color, visited)
+        return visited
+    
+    def _traversal(self, row, col, color) -> set:
+        if(row >= 0 and row < 9 and col >= 0 and col < 9 and self.board[row][col] == color):
+            visited = set()
+            return self._traversal_helper(row, col, color, visited)
+        return set()
 
 
 
 def main():
     b = Board()
     b.place(1, 1)
-    b.place(1,2)
-    b.place(2,1)
+    b.place(1, 2)
+    b.place(2, 1)
+    b.place(0, 1)
+    b.place(0, 2)
+    b.place(0, 0)
+    b.place(3, 5)
+    b.place(1, 0)
+    empty = set()
+    empty = b._traversal(0, 0, 1)
     print(b)
-    
+    print(", ".join(str(element) for element in empty))
     
 
 
