083b423acaebf22704f883c4d8167045a5f41aa9
Ibubasid1 <siddiqui.muhibullah@gmail.com>
2026-04-30 19:02:21 +0500

    Removed driver/temporary code from random agent

--- src/random_agent.py
+++ src/random_agent.py
@@ -13,20 +13,3 @@ class RandomAgent:
             board.skip()
         else:
             board.place(pick[0], pick[1])
-
-        
-b = Board()
-agent1 = RandomAgent(1)
-agent2 = RandomAgent(2)
-print("Starting game!")
-x = 0
-while(not b.game_over):
-    x += 1
-    agent1.make_move(b)
-    print("Player 1 move: ")
-    print(b)
-    agent2.make_move(b)
-    print("Player 2 move: ")
-    print(b)
-
-print("Game finished")
