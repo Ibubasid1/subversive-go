93acd3071c9890e899b4b18c66bf932e8426c5c5
Dilair512 <m.shahmeerimran2201099@gmail.com>
2026-05-06 13:13:49 +0500

    Add files via upload

--- src/board.py
+++ src/board.py
@@ -1,242 +1,102 @@
-class Board:
+import copy
 
+
+class Board:
     SIZE = 9
 
     def __init__(self):
-        self.board = [[0]*Board.SIZE for _ in range(Board.SIZE)]
+        self.board = [[0] * Board.SIZE for _ in range(Board.SIZE)]
         self.moves = 0
         self.last_was_pass = False
         self.current_player = 1
         self.game_over = False
         self.black_score = 0
-        self.white_score = 6.5 #starts with komi bonus
+        self.white_score = 6.5
 
-
-    #retrieves all adjacent pieces to the provided piece
     def get_adj(self, row, col):
         adjacent = set()
-        if row < 8:
-            adjacent.add((row + 1, col))
-        if row > 0:
-            adjacent.add((row - 1, col))
-        if col < 8:
-            adjacent.add((row, col + 1))
-        if col > 0:
-            adjacent.add((row, col - 1))
+        if row < 8: adjacent.add((row + 1, col))
+        if row > 0: adjacent.add((row - 1, col))
+        if col < 8: adjacent.add((row, col + 1))
+        if col > 0: adjacent.add((row, col - 1))
         return adjacent
-    
+
     def skip(self):
-        if self.last_was_pass: #checks if opponent has already passed
-            self.game_over = True #if opponent has, then the game should end
-        else: #if the opponent hasn't, then the game continues 
+        if self.last_was_pass:
+            self.game_over = True
+        else:
             self.moves += 1
             self.last_was_pass = True
-        self.current_player = 2 if self.current_player == 1 else 1
         if self.game_over:
             self._announce_winner()
-    
-    
-    @property
-    def is_terminal(self):
-        return self.game_over()
-    
 
-    def is_legal(self, row, col, color):
-        result = True
+    def place(self, row, col):
         if self.board[row][col] != 0:
             return False
-        self.board[row][col] = color
-        adjacents = self.get_adj(row, col)
 
-        for element in adjacents:
-            if self.count_liberties(element[0], element[1]) == 0 and self.board[element[0]][element[1]] != color:
-                result = True
-        if self.count_liberties(row, col) == 0:
-            result = False
-
-        self.board[row][col] = 0
-        return result
-    
-
-    def legal_moves(self, color) -> set:
-        #none represents 'pass' 
-        all_legal_moves = set()
-        all_legal_moves.add(None)
-        for r in range(9):
-            for c in range(9):
-                if self.is_legal(r, c, color):
-                    all_legal_moves.add((r, c))
-        return all_legal_moves
-
-
-    def place(self, row, col): #uses a simply row and column to make the move
-        #make a check to see whether the move is available
-        if not self.is_legal(row, col, self.current_player):
-            # print("Invalid move")
-            print(f"Invalid move at ({row}, {col}) for player {self.current_player}")
-            return
-        else:
-            self.board[row][col] = self.current_player
-
+        self.board[row][col] = self.current_player
         temp = self.get_adj(row, col)
 
         for element in temp:
-            if self.count_liberties(element[0], element[1]) == 0 and self.board[element[0]][element[1]] != self.current_player:
+            if self.count_liberties(element[0], element[1]) == 0 and self.board[element[0]][
+                element[1]] != self.current_player:
                 removable = self._traversal(element[0], element[1], self.board[element[0]][element[1]])
                 for item in removable:
                     self.board[item[0]][item[1]] = 0
 
+        if self.count_liberties(row, col) == 0:
+            self.board[row][col] = 0
+            return False
+
         self.last_was_pass = False
-
         self.moves += 1
-
         self.current_player = 2 if self.current_player == 1 else 1
 
         if self.moves >= 400: self.game_over = True
+        return True
 
-        if self.game_over:
-            self._announce_winner()
-
-
-    def __str__(self):
-        rows = []
-        for row in self.board:
-            rows.append("|".join(str(cell) for cell in row))
-        return "\n".join(rows)
+    def get_legal_moves(self):
+        legal = []
+        for r in range(self.SIZE):
+            for c in range(self.SIZE):
+                if self.board[r][c] == 0:
+                    test_board = copy.deepcopy(self)
+                    if test_board.place(r, c):
+                        legal.append((r, c))
+        return legal
 
     def _traversal_helper(self, row, col, color, visited) -> set:
         visited.add((row, col))
-        if (row + 1) < self.SIZE and self.board[row + 1][col] == color and (row + 1, col) not in visited:
-            self._traversal_helper(row + 1, col, color, visited)
-        if (row - 1) >= 0 and self.board[row - 1][col] == color and (row - 1, col) not in visited:
-            self._traversal_helper(row - 1, col, color, visited)
-        if (col + 1) < self.SIZE and self.board[row][col + 1] == color and (row, col + 1) not in visited:
-            self._traversal_helper(row, col + 1, color, visited)
-        if (col - 1) >= 0 and self.board[row][col - 1] == color and (row, col - 1) not in visited:
-            self._traversal_helper(row, col - 1, color, visited)
+        for r, c in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
+            if 0 <= r < self.SIZE and 0 <= c < self.SIZE:
+                if self.board[r][c] == color and (r, c) not in visited:
+                    self._traversal_helper(r, c, color, visited)
         return visited
 
-    #main traversal function, used so that the 'visited' set is not reset on every instance of recursion
     def _traversal(self, row, col, color) -> set:
-        #checks for bounds and ensures color is the same as the piece currently in that position
-        if(row >= 0 and row < 9 and col >= 0 and col < 9 and self.board[row][col] == color):
-            visited = set()
-            return self._traversal_helper(row, col, color, visited)
+        if 0 <= row < 9 and 0 <= col < 9 and self.board[row][col] == color:
+            return self._traversal_helper(row, col, color, set())
         return set()
-    
+
     def count_liberties(self, row, col) -> int:
-        visited = set()
-        connected = self._traversal(row, col, self.board[row][col])
-        for element in connected:
-            r = element[0]
-            c = element[1]
-            #no need for a 'not in visited' check since if it was already in visited it would simply not be added again. sets hold unique values
-            if r < 8 and self.board[r + 1][c] == 0:
-                visited.add((r + 1, c))
-            if r > 0 and self.board[r - 1][c] == 0:
-                visited.add((r - 1, c))
-            if c < 8 and self.board[r][c + 1] == 0:
-                visited.add((r, c + 1))
-            if c > 0 and self.board[r][c - 1] == 0:
-                visited.add((r, c - 1))   
-        return len(visited)
-    
+        liberties = set()
+        group = self._traversal(row, col, self.board[row][col])
+        for r, c in group:
+            for adj_r, adj_c in [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]:
+                if 0 <= adj_r < 9 and 0 <= adj_c < 9 and self.board[adj_r][adj_c] == 0:
+                    liberties.add((adj_r, adj_c))
+        return len(liberties)
+
     def scoring(self):
         self.black_score = 0
         self.white_score = 6.5
-        total_visited = set()
-        all_positions = {(r, c) for r in range(9) for c in range(9)}
-        black_list = set()
-        white_list = set()
-
-        for element in all_positions:
-            if self.board[element[0]][element[1]] == 1:
-                black_list.add(element)
-                self.black_score += 1
-            elif self.board[element[0]][element[1]] == 2:
-                white_list.add(element)
-                self.white_score += 1
-        
-        all_positions -= black_list
-        all_positions -= white_list
-        total_visited.update(black_list)
-        total_visited.update(white_list)
-
-        for element in all_positions:
-            if element not in total_visited:
-                adjacents = set()
-                open_space = self._traversal(element[0], element[1], self.board[element[0]][element[1]])
-                total_visited.update(open_space)
-                for item in open_space:
-                    temp = self.get_adj(item[0], item[1])
-                    for thing in temp:
-                        if self.board[thing[0]][thing[1]] != 0:
-                            adjacents.add(thing)
-                if not adjacents:
-                    continue
-                chosen = next(iter(adjacents))
-                piece_type = self.board[chosen[0]][chosen[1]]
-                all_same = True
-                for item in adjacents:
-                    if self.board[item[0]][item[1]] != piece_type:
-                        all_same = False
-                if all_same:
-                    if piece_type == 1:
-                        self.black_score += len(open_space)
-                    else:
-                        self.white_score += len(open_space)
+        for r in range(9):
+            for c in range(9):
+                if self.board[r][c] == 1:
+                    self.black_score += 1
+                elif self.board[r][c] == 2:
+                    self.white_score += 1
 
     def _announce_winner(self):
         self.scoring()
-        print("Player 1 has " + str(self.black_score) + "!")
-        print("Player 2 has " + str(self.white_score) + "!")
-        if(self.white_score > self.black_score):
-            print("Player 2 wins!")
-        else:
-            print("Player 1 wins!")
-            
-            
-    def get_value(self, piece):
-        if self.is_terminal:
-            if piece == 1:
-                return self.black_score
-            return self.white_score;  
-
-
-
-def main():
-    b = Board()
-    b.place(1, 1) #1
-    b.place(1, 2) #2
-    b.place(2, 1) #1
-    b.place(0, 1) #2
-    b.place(0, 2) #1
-    b.place(0, 0) #2
-    b.place(3, 5) #1 
-    b.place(1, 0) #2
-    print(b)
-    print()
-    b.place(2, 0) #1
-    b.place(7, 7) #2
-    b.place(0, 0) #1
-    print(b.is_legal(0, 1, 2))
-    print(b.is_legal(1, 0, 2))
-    print(b.is_legal(0, 1, 1))
-    print(b.is_legal(1, 0, 1))
-    print(b)
-
-
-
-
-
-    #if even number of moves, then black turn
-    #if odd number of moves, then white turn
-    #two possible moves, place or pass (maybe resign later)
-    #let's call move 1 place and move 2 pass. If there are two consecutive move 2s, the game ends. 
-    #however, the ai will likely not willingly pass. The random move will definitely not since that won't even be an option.
-    #so, it's more likely that the game will end by the move limit we set, which will be 400
-
-    #have created a new branch for this code, which will specifically be restricted to this file
-
-if __name__ == "__main__":
-    main()
+        print(f"P1: {self.black_score}, P2: {self.white_score}")
--- src/board_gui.py
+++ src/board_gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from board import Board
+from ai_agent import AlphaBetaAgent
 
 SIZE = 9
 CELL = 60
@@ -7,36 +8,45 @@ MARGIN = CELL
 
 
 class GoGUI:
-
     def __init__(self):
         self.game = Board()
-
+        self.ai = AlphaBetaAgent(depth=2)
         self.root = tk.Tk()
-        self.root.title("Go Game")
+        self.root.title("Go AI - AlphaBeta")
 
         canvas_size = 2 * MARGIN + (SIZE - 1) * CELL
-
         self.turn_label = tk.Label(self.root, text="Black's Turn", font=("Arial", 14))
         self.turn_label.pack(pady=5)
 
         self.canvas = tk.Canvas(self.root, width=canvas_size, height=canvas_size, bg="beige")
         self.canvas.pack()
 
-        self.pass_button = tk.Button(self.root, text="Pass", command=self.on_pass)
-        self.pass_button.pack(pady=5)
+        btn_frame = tk.Frame(self.root)
+        btn_frame.pack(pady=5)
+
+        tk.Button(btn_frame, text="Pass", command=self.on_pass).pack(side=tk.LEFT, padx=5)
+        tk.Button(btn_frame, text="AI Move (Current Side)", command=self.run_ai).pack(side=tk.LEFT, padx=5)
 
         self.canvas.bind("<Button-1>", self.on_click)
         self.draw_board()
 
-    def _update_banner(self):
-        if self.game.current_player == 1:
-            self.turn_label.config(text="Black's Turn")
+    def run_ai(self):
+        is_maximizing = True if self.game.current_player == 1 else False
+        _, move = self.ai.search(self.game, self.ai.depth, float('-inf'), float('inf'), is_maximizing)
+
+        if move:
+            self.game.place(move[0], move[1])
+            self.draw_board()
+            self._update_banner()
         else:
-            self.turn_label.config(text="White's Turn")
+            self.on_pass()
+
+    def _update_banner(self):
+        color = "Black" if self.game.current_player == 1 else "White"
+        self.turn_label.config(text=f"{color}'s Turn")
 
     def draw_board(self):
         self.canvas.delete("all")
-
         for i in range(SIZE):
             x = MARGIN + i * CELL
             y = MARGIN + i * CELL
@@ -46,48 +56,22 @@ class GoGUI:
         for row in range(SIZE):
             for col in range(SIZE):
                 piece = self.game.board[row][col]
-                if piece == 0:
-                    continue
-                x = MARGIN + col * CELL
-                y = MARGIN + row * CELL
-                radius = CELL // 2 - 4
+                if piece == 0: continue
+                x, y = MARGIN + col * CELL, MARGIN + row * CELL
                 color = "black" if piece == 1 else "white"
-                self.canvas.create_oval(x - radius, y - radius, x + radius, y + radius, fill=color)
+                self.canvas.create_oval(x - 25, y - 25, x + 25, y + 25, fill=color)
 
     def on_click(self, event):
-        if self.game.game_over:
-            return
         col = round((event.x - MARGIN) / CELL)
         row = round((event.y - MARGIN) / CELL)
-        if not (0 <= row < SIZE and 0 <= col < SIZE):
-            return
-        old_board = [r[:] for r in self.game.board]
-        self.game.place(row, col)
-        if old_board != self.game.board:
-            self.draw_board()
-            self._update_banner()
-        if self.game.game_over:
-            self.show_winner()
+        if 0 <= row < SIZE and 0 <= col < SIZE:
+            if self.game.place(row, col):
+                self.draw_board()
+                self._update_banner()
 
     def on_pass(self):
-        if self.game.game_over:
-            return
         self.game.skip()
         self._update_banner()
-        if self.game.game_over:
-            self.show_winner()
-
-    def show_winner(self):
-        self.game.scoring()
-        black = self.game.black_score
-        white = self.game.white_score
-        winner = "Black" if black > white else "White"
-
-        top = tk.Toplevel(self.root)
-        top.title("Game Over")
-        tk.Label(top, text=f"Black: {black}   White: {white}", font=("Arial", 12)).pack(pady=20,padx=20)
-        tk.Label(top, text=f"{winner} wins!", font=("Arial", 14, "bold")).pack(pady=10)
-        tk.Button(top, text="Close", command=top.destroy).pack(pady=10)
 
     def run(self):
         self.root.mainloop()
